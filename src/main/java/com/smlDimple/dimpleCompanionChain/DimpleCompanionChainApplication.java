f337538d190c39fe524e76e5b839e2379f94be76
dimple <2250434308@qq.com>
2024-06-03 22:48:41 +0800

    第一次初始化项目

---
+++ src/main/java/com/smlDimple/dimpleCompanionChain/DimpleCompanionChainApplication.java
@@ -0,0 +1,13 @@
+package com.smlDimple.dimpleCompanionChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimpleCompanionChainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DimpleCompanionChainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smlDimple/dimpleCompanionChain/DimpleCompanionChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smlDimple.dimpleCompanionChain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DimpleCompanionChainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
