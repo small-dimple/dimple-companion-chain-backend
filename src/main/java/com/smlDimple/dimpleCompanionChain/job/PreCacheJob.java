5ade1c2b0e23ba2006422a4975d13821f8006fb0
dimple <2250434308@qq.com>
2024-06-14 01:55:45 +0800

    add 添加 Redisson 分布式锁

--- src/main/java/com/smlDimple/dimpleCompanionChain/job/PreCacheJob.java
+++ src/main/java/com/smlDimple/dimpleCompanionChain/job/PreCacheJob.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.smlDimple.dimpleCompanionChain.model.domain.User;
 import com.smlDimple.dimpleCompanionChain.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -29,29 +31,45 @@ public class PreCacheJob {
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private RedissonClient redissonClient;
+
     //重点用户
     private List<Long> mainUserList = List.of(1L);
 
     @Scheduled(cron = "0 0 0 * * ?") //每天凌晨执行
     public void doCacheRecommendUser(){
+        RLock lock = redissonClient.getLock("dimple:precachejob:docache:lock");
+        try {
+            if(lock.tryLock(0,30000L, TimeUnit.MILLISECONDS))
+            {
+                for(long userId: mainUserList){
+                    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+                    Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
 
-        for(long userId: mainUserList){
-            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-            Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
-
-            String redisKey = String.format("dimple:user:recommend:%s",userId);
-            ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+                    String redisKey = String.format("dimple:user:recommend:%s",userId);
+                    ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
 
 
-            try {
-            /*
-            ！！！！！！！！！！！！！一定要设置过期时间
-             */
-                valueOperations.set(redisKey,userPage,30000, TimeUnit.MILLISECONDS);
-            } catch (Exception e) {
-                log.error("redis set key error",e);
+                    try {
+                /*
+                ！！！！！！！！！！！！！一定要设置过期时间
+                 */
+                        valueOperations.set(redisKey,userPage,30000, TimeUnit.MILLISECONDS);
+                    } catch (Exception e) {
+                        log.error("redis set key error",e);
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            log.error("doCacheRecommendUser error",e);
+        }finally {
+            if(lock.isHeldByCurrentThread()){
+                lock.unlock();
             }
         }
+
 
     }
 }
